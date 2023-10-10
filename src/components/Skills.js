b9111a6355c222748ab08af9f0491537dfcb7f4e
rabin9087_Github <rabin9087@gmail.com>
2023-10-10 14:02:18 +1100

    skills file changed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,21 +6,21 @@ const Skills = () => {
         <h2 className="title">My Skills</h2>
         <div>
 
-      <div className=""style={{background: "yellow"}}>
+      <div className="">
         
-      <i class="fa-brands fa-html5" style={{background: "yellow"}}></i>
+      <i class="fa-brands fa-html5" style={{color: "#E75D28"}}></i>
         <span>HTML</span>
       </div>
       <div className="">
-      <i class="fa-brands fa-css3-alt"></i>
+      <i class="fa-brands fa-css3-alt" style={{color: "#254CDD"}}></i>
         <span>CSS</span>
       </div>
       <div className="">
-        <i class="fa-brands fa-js"></i>
+        <i class="fa-brands fa-js" style={{color: "#EFD81D"}}></i>
         <span>Javascript</span>
       </div>
       <div className="">
-        <i class="fa-brands fa-react"></i>
+        <i class="fa-brands fa-react" style={{color: "#5ED4F3"}}></i>
         <span>React</span>
       </div>
       </div>
